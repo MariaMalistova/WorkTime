3103598c37b1017fe8445d70e9f7aa9f895a2d49
MariaMalistova <50328876+MariaMalistova@users.noreply.github.com>
2024-09-08 22:46:52 +0300

    first upload

---
+++ main.js
@@ -0,0 +1,50 @@
+$(function() {
+    console.log( "ready!" );
+    let duration = 2;
+    let start = 20;
+    makeAll(start);
+});
+
+function makeAll(start) {
+    setInterval(() => {
+        $('#countdown').text(loadTime(start));
+    }, "1000");
+}
+
+function loadTime(start) {
+    let now = new Date();
+    let hoursNow = now.getHours();
+    let nowInt = now.getTime();
+    //let nowInt = now.setHours(10, 21, 45);
+    let startDate = new Date().setHours(start, 0, 0);
+    let endDate = new Date().setHours(start + duration, 0, 0);
+    if (hoursNow > (start + duration) && hoursNow < start) {
+        return 'Конец дня!';
+    } else {
+        var _second = 1000;
+        var _minute = _second * 60;
+        var _hour = _minute * 60;
+
+        let lasts = endDate - nowInt;
+        let hours = Math.floor(lasts / _hour);
+        let minutes = Math.floor((lasts % _hour) / _minute);
+        return `${hours} ч ${minutes} мин`;
+    }
+    /*var distance = end - now;
+    if (distance < 0) {
+
+        clearInterval(timer);
+        document.getElementById('countdown').innerHTML = 'EXPIRED!';
+
+        return;
+    }
+    var days = Math.floor(distance / _day);
+    var hours = Math.floor((distance % _day) / _hour);
+    var minutes = Math.floor((distance % _hour) / _minute);
+    var seconds = Math.floor((distance % _minute) / _second);
+
+    document.getElementById('countdown').innerHTML = days + 'days ';
+    document.getElementById('countdown').innerHTML += hours + 'hrs ';
+    document.getElementById('countdown').innerHTML += minutes + 'mins ';
+    document.getElementById('countdown').innerHTML += seconds + 'secs'*/
+}
