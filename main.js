228fb9fc3852e7a387cfd50b79610c738c239f93
MariaMalistova <50328876+MariaMalistova@users.noreply.github.com>
2024-09-10 12:42:08 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,23 +3,20 @@ let start = 8;
 
 $(function() {
     console.log( "ready!" );
-    makeAll(start);
+    let timer = setInterval(() => {
+        let now = new Date();
+        let hoursNow = now.getHours();
+        let nowInt = now.getTime();
+        //let nowInt = now.setHours(11, 21, 45);
+        let startDate = new Date().setHours(start, 0, 0);
+        let endDate = new Date().setHours(start + duration, 0, 0);
+        $('#countdown').text(loadTime(hoursNow, nowInt, endDate));
+        progressBar(startDate, start, hoursNow, nowInt, endDate);
+    }, "1000");
 });
 
-function makeAll(start) {
-    setInterval(() => {
-        $('#countdown').text(loadTime(start));
-    }, "1000");
-}
-
-function loadTime(start) {
-    let now = new Date();
-    let hoursNow = now.getHours();
-    let nowInt = now.getTime();
-    //let nowInt = now.setHours(10, 21, 45);
-    let startDate = new Date().setHours(start, 0, 0);
-    let endDate = new Date().setHours(start + duration, 0, 0);
-    if (hoursNow > (start + duration) || hoursNow < start) {
+function loadTime(hoursNow, nowInt, endDate) {
+    if (hoursNow >= (start + duration) || hoursNow < start) {
         return 'Конец дня!';
     } else {
         var _second = 1000;
@@ -29,24 +26,18 @@ function loadTime(start) {
         let lasts = endDate - nowInt;
         let hours = Math.floor(lasts / _hour);
         let minutes = Math.floor((lasts % _hour) / _minute);
-        let persents = ((nowInt - startDate) / (endDate - startDate) * 100).toFixed(2);
-        return `${hours} ч ${minutes} мин, ${persents}%`;
+        return `${hours} ч ${minutes} мин`;
     }
-    /*var distance = end - now;
-    if (distance < 0) {
+}
 
-        clearInterval(timer);
-        document.getElementById('countdown').innerHTML = 'EXPIRED!';
-
-        return;
+function progressBar(startDate, start, hoursNow, nowInt, endDate) {
+    if (hoursNow >= (start + duration) || hoursNow < start) {
+        elem.style.width = "100%";
+        elem.innerHTML = "100%";
+    } else {
+        let width = ((nowInt - startDate) / (endDate - startDate) * 100).toFixed(2);
+        var elem = document.getElementById("progressBar");
+        elem.style.width = width + "%";
+        elem.innerHTML = width  + "%";
     }
-    var days = Math.floor(distance / _day);
-    var hours = Math.floor((distance % _day) / _hour);
-    var minutes = Math.floor((distance % _hour) / _minute);
-    var seconds = Math.floor((distance % _minute) / _second);
-
-    document.getElementById('countdown').innerHTML = days + 'days ';
-    document.getElementById('countdown').innerHTML += hours + 'hrs ';
-    document.getElementById('countdown').innerHTML += minutes + 'mins ';
-    document.getElementById('countdown').innerHTML += seconds + 'secs'*/
 }
